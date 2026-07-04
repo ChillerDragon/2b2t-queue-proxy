ddc7dfaf3d58f1dfbf1fae046f3849c24c1af93f
ChillerDragon <ChillerDragon@gmail.com>
2026-07-04 19:12:02 +0200

    connecting kinda works i think

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 const mineflayer = require('mineflayer')
 
+require('dotenv').config({ debug: false })
+
+if(typeof process.env.MC_USERNAME != 'string') {
+  console.log("You have to set MC_USERNAME in your .env file")
+  process.exit(1)
+}
+
+console.log(`Usine minecraft account '${process.env.MC_USERNAME}'`)
+
 const bot = mineflayer.createBot({
-  host: 'localhost',
-  username: 'Bot',
-  auth: 'microsoft'
-  // port: 25565,              // set if you need a port that isn't 25565
-  // version: false,           // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
+  host: process.env.SERVER_HOST || '2b2t.org',
+  username: process.env.MC_USERNAME,
+  auth: 'microsoft',
+  // version: false, // TODO: set this to 2b2t version
   // password: '12345678'      // set if you want to use password-based auth (may be unreliable). If specified, the `username` must be an email
 })
 
 bot.on('chat', (username, message) => {
-  if (username === bot.username) return
-  bot.chat(message)
+  console.log(`[chat] ${username}: ${message}`)
 })
 
-// Log errors and kick reasons:
 bot.on('kicked', console.log)
 bot.on('error', console.log)
