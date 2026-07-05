374bd1dfff65e4f75a5ecbcf2916a209ed4a25d0
ChillerDragon <chillerdragon@gmail.com>
2026-07-05 12:04:37 +0200

    cant get the official server example from the readme to work

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const bot = mineflayer.createBot({
   host: process.env.SERVER_HOST || '2b2t.org',
   username: process.env.MC_USERNAME,
   auth: 'microsoft',
-  // version: false, // TODO: set this to 2b2t version
+  // version: false, // TODO: set this to 2b2t version which is 1.21.4
   // password: '12345678'      // set if you want to use password-based auth (may be unreliable). If specified, the `username` must be an email
 })
 
@@ -23,3 +23,74 @@ bot.on('chat', (username, message) => {
 
 bot.on('kicked', console.log)
 bot.on('error', console.log)
+
+// ------------
+
+const mc = require('minecraft-protocol')
+const nbt = require('prismarine-nbt')
+const server = mc.createServer({
+  'online-mode': false,   // optional
+  host: '0.0.0.0',       // optional
+  port: 25565,           // optional
+  version: '1.21.4'
+})
+const mcData = require('minecraft-data')(server.version)
+
+function chatText (text) {
+  return mcData.supportFeature('chatPacketsUseNbtComponents')
+    ? nbt.comp({ text: nbt.string(text) })
+    : JSON.stringify({ text })
+}
+
+server.on('playerJoin', function(client) {
+  const loginPacket = mcData.loginPacket
+
+  client.write('login', {
+    ...loginPacket,
+    enforceSecureChat: false,
+    entityId: client.id,
+    hashedSeed: [0, 0],
+    maxPlayers: server.maxPlayers,
+    viewDistance: 10,
+    reducedDebugInfo: false,
+    enableRespawnScreen: true,
+    isDebug: false,
+    isFlat: false
+  })
+
+  client.write('position', {
+    x: 0,
+    y: 255,
+    z: 0,
+    yaw: 0,
+    pitch: 0,
+    flags: 0x00
+  })
+
+  const message = {
+    translate: 'chat.type.announcement',
+    with: [
+      'Server',
+      'Hello, world!'
+    ]
+  }
+  if (mcData.supportFeature('signedChat')) {
+    client.write('player_chat', {
+      plainMessage: message,
+      signedChatContent: '',
+      unsignedChatContent: chatText(message),
+      type: mcData.supportFeature('chatTypeIsHolder') ? { chatType: 1 } : 0,
+      senderUuid: 'd3527a0b-bc03-45d5-a878-2aafdd8c8a43', // random
+      senderName: JSON.stringify({ text: 'me' }),
+      senderTeam: undefined,
+      timestamp: Date.now(),
+      salt: 0n,
+      signature: mcData.supportFeature('useChatSessions') ? undefined : Buffer.alloc(0),
+      previousMessages: [],
+      filterType: 0,
+      networkName: JSON.stringify({ text: 'me' })
+    })
+  } else {
+    client.write('chat', { message: JSON.stringify({ text: message }), position: 0, sender: 'me' })
+  }
+})
